417d76c28832e6008e15242523b434b5bbb4824b
Sorab828 <sorab.gla_cs.aiml24@gla.ac.in>
2026-07-04 22:40:39 +0530

    Add new pattern

--- ReverseRightAngleTriangle.java
+++ ReverseRightAngleTriangle.java
@@ -1,11 +1,11 @@
 import java.util.*;
-public class Number2{
+public class ReverseRightAngleTriangle{
     public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
     for(int i=1;i<=n;i++){
-        for(int j=1;j<=i;j++){
-            System.out.print(i);
+        for(int j=1;j<=n-i+1;j++){
+            System.out.print("*");
         }
         System.out.println();
     }
