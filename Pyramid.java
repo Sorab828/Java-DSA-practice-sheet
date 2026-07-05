8da997723f638c49580fc1e47dde5ccdb806543c
Sorab828 <sorab.gla_cs.aiml24@gla.ac.in>
2026-07-05 14:31:26 +0530

    Add a new pattern pyramid

--- Pyramid.java
+++ Pyramid.java
@@ -1,15 +1,18 @@
 import java.util.*;
-
-public class ReverseNumber1 {
+public class Pyramid {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         int n = sc.nextInt();
 
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n-i; j++) {
+            for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
+
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+
             System.out.println();
         }
     }
